a59add5bc0926fb3327507319e1591f3d955b3a3
djolivier <y.karasangabo@bg-rams.ac.at>
2013-11-25 20:36:17 +0100

    some changes

---
+++ cwm/step3.sh
@@ -0,0 +1,23 @@
+#!/data/local/tmp/cwm/sh
+
+echo remount rw /system
+mount -o remount rw /system
+
+echo copy recovery.tar to system.
+cat /data/local/tmp/cwm/recovery.tar > /system/bin/recovery.tar
+chmod 644 /system/bin/recovery.tar
+
+echo copy chargemon to system.
+cat /data/local/tmp/cwm/chargemon > /system/bin/chargemon
+chmod 755 /system/bin/chargemon
+
+echo copy charger to system.
+cat /data/local/tmp/cwm/charger > /system/bin/charger
+chmod 755 /system/bin/charger
+
+echo copy sh to system.
+cat /data/local/tmp/cwm/sh > /system/xbin/sh
+chmod 755 /system/xbin/sh
+
+echo remount ro /system
+mount -o remount ro /system
